70fc33db9a8c428fde5d7d632eff0301dd4afd77
asepridwan1989 <aseprmail@gmail.com>
2018-04-02 12:43:54 +0700

    ubah nama method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function to_roman (num) {
+function toRoman (num) {
   var roman = [[1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],[100, 'C'],[90, 'XC'],
   [50, 'L'],[40, 'XL'],[10, 'X'],[9, 'IX'],[5, 'V'],[4, 'IV'],[1, 'I']]
   var result = '';
@@ -16,8 +16,8 @@ function to_roman (num) {
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|———————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
-console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('4     | IV       | ', toRoman(4))
+console.log('9     | IX       | ', toRoman(9))
+console.log('13    | XIII     | ', toRoman(13))
+console.log('1453  | MCDLIII  | ', toRoman(1453))
+console.log('1646  | MDCXLVI  | ', toRoman(1646))
